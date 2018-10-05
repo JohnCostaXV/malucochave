b2ce19f4d0d2fb09881dca34c2483373bd22e1d9
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-10-04 21:55:14 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import discord
+import asyncio
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Iniciado com sucesso!")
+    while True:
+        await client.change_presence(game=discord.Game(name="Online com mais de {} membros!".format(str(len(set(client.get_all_members())))), url="https://www.twitch.tv/johncostaxv", type=1))
+        await asyncio.sleep(300)
+        await client.change_presence(game=discord.Game(name="PerfectNetwork!\nCriado por Johnn#0001", url="https://www.twitch.tv/johncostaxv", type=1))
+        await asyncio.sleep(300)
+        await client.change_presence(game=discord.Game(name="mc-perfect.com.br", url="https://www.twitch.tv/johncostaxv", type=1))
+        await asyncio.sleep(300)
+
+client.run(os.environ.get("BOT_TOKEN"))
