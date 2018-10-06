24bf3f64e1ccfeb8913205fb7510716cc484189b
JohnCostaXV <38146087+JohnCostaXV@users.noreply.github.com>
2018-10-05 22:32:14 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -181,7 +181,7 @@ async def on_reaction_add(reaction, user):
      await client.remove_roles(user, role1)
      
      await asyncio.sleep(1)
-     role = discord.utils.get(user.server.roles, name="👨🏻‍🚀 JOGADOR")
+     role = discord.utils.get(user.server.roles, name="👨🏻‍🚀 Jogador")
      await client.add_roles(user, role)
 
 
